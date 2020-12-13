c7741944fab1af5a1d20f1cd6c098872aa7ca411
MarijanOrsolic <mmarijan.orsolic@gmail.com>
2020-12-13 14:40:34 +0100

    Initial commit

---
+++ app/src/main/java/ba/sum/fpmoz/morsolic/pma/model/Student.java
@@ -0,0 +1,13 @@
+package ba.sum.fpmoz.morsolic.pma.model;
+
+public class Student {
+    public String uid;
+    public String name;
+    public String surname;
+
+    public Student(String uid, String name, String surname) {
+        this.uid = uid;
+        this.name = name;
+        this.surname = surname;
+    }
+}
